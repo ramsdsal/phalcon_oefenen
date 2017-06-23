86fcc9c0a6ea194207e1ef8737c5a22c754fdfff
ramsdsal <ramsdsal@hotmail.com>
2017-06-23 21:00:56 +0200

    Create the boostrap file

--- public/index.php
+++ public/index.php
@@ -1,1 +1,31 @@
-<?php phpinfo();
+<?php
+try
+{
+	//Create a loader
+	$loader = new \Phalcon\Loader();
+	$loader->registerDirs([
+		'../app/controllers/',
+		'../app/models/',
+		'../app/views/'
+	]);
+	$loader->register();
+
+	//Dependency injection
+	$di = new \Phalcon\DI\FactoryDefault();
+	$di->set('view',function(){
+		$view = new \Phalcon\Mvc\View();
+		$view->setViewsDir('../app/views/');
+		return $view;
+	});
+
+	//Deploy the app
+	$app = new \Phalcon\Mvc\Application($di);
+	echo $app->handle()->getContent();
+}
+catch(\Phalcon\Exception $e)
+{
+	echo $e->getMessage();
+};
+
+
+?>
